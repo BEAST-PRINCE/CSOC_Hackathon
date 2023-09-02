a59afda9eee061eca5f005c3657d6a0077cd3ee8
Divyanshu Prince <divyanshuprince764@gmail.com>
2023-09-02 11:29:26 +0530

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: ,
+      home: null,
     );
   }
 }
